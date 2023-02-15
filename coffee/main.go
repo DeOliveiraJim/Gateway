5a24471f1b8fecb06bd80c9d4063b1785f3f4b3a
JessyHc <jessy.hoche@univ-rouen.fr>
2023-02-15 15:36:08 +0100

    ajout endpoint avec le paramètre dans le l'url

--- coffee/main.go
+++ coffee/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func carrierHandler(w http.ResponseWriter, r *http.Request) {
@@ -19,6 +20,16 @@ func carrierHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("User  " + servant + " is accessing MS Carrier ! first = " + first + "second = " + second))
 }
 
+func carrierTypeHandler(w http.ResponseWriter, r *http.Request) {
+    servant, err := os.Hostname()
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+    param := strings.TrimPrefix(r.URL.Path, "/test/")
+    w.Write([]byte("User  " + servant + " is accessing MS Carrier ! param = {" + param + "}"))
+}
+
 const keyServerAddr = "server 127.0.0.1"
 
 func String(any any) {
@@ -27,5 +38,6 @@ func String(any any) {
 
 func main() {
 	http.HandleFunc("/test", carrierHandler)
+	http.HandleFunc("/test/", carrierTypeHandler)
 	log.Fatal(http.ListenAndServe(":8181", nil))
 }
