b42fb7d60c17b49bcf7a4ab918ebc9a5c153cda7
De Oliveira <D@rkted27>
2023-02-16 14:36:34 +0100

    changement de errand et planner

--- errand/main.go
+++ errand/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"regexp"
 )
 
 func errandGetHandler(w http.ResponseWriter, r *http.Request) {
@@ -32,6 +34,23 @@ func errandPatchHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("User  " + servant + " is accessing MS Errand ! first = " + first + "second = " + second))
 }
 
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+	// Récupération de l'URL
+	url := r.URL.Path
+
+	// Expression régulière pour extraire l'id
+	re := regexp.MustCompile(`^/carrier/(\d+)/itinerary$`)
+	matches := re.FindStringSubmatch(url)
+
+	// Si l'URL correspond au modèle attendu, on extrait l'id et on l'affiche
+	if len(matches) == 2 {
+		id := matches[1]
+		fmt.Fprintf(w, "L'id est : %s", id)
+	} else {
+		http.NotFound(w, r)
+	}
+}
+
 const keyServerAddr = "server 127.0.0.1"
 
 func String(any any) {
@@ -41,5 +60,6 @@ func String(any any) {
 func main() {
 	http.HandleFunc("/get", errandGetHandler)
 	http.HandleFunc("/patch", errandPatchHandler)
+	http.HandleFunc("/", handleRequest)
 	log.Fatal(http.ListenAndServe(":8282", nil))
 }
--- planner/main.go
+++ planner/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -47,6 +49,24 @@ func plannerPatchHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("PATCH REQUEST , name = " + value))
 }
 
+func getBodyAndHeader(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("server: %s /\n", r.Method)
+	fmt.Printf("server: query id: %s\n", r.URL.Query().Get("id"))
+	fmt.Printf("server: content-type: %s\n", r.Header.Get("content-type"))
+	fmt.Printf("server: headers:\n")
+	for headerName, headerValue := range r.Header {
+		fmt.Printf("\t%s = %s\n", headerName, strings.Join(headerValue, ", "))
+	}
+
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Printf("server: could not read request body: %s\n", err)
+	}
+	fmt.Printf("server: request body: %s\n", reqBody)
+
+	fmt.Fprintf(w, `{"message": "hello!"}`)
+}
+
 const keyServerAddr = "server 127.0.0.1"
 
 func String(any any) {
@@ -57,5 +77,6 @@ func main() {
 	http.HandleFunc("/get/", plannerGetHandler)
 	http.HandleFunc("/post/", plannerPostHandler)
 	http.HandleFunc("/patch/", plannerPatchHandler)
+	http.HandleFunc("/", getBodyAndHeader)
 	log.Fatal(http.ListenAndServe(":8585", nil))
 }
